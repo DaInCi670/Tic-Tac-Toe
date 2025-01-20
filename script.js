691ee3b15833d17b8ccbc09872f9933aaad15c46
Emmanuel Yeboah <105175968+DaInCi670@users.noreply.github.com>
2025-01-20 07:09:23 +0000

    update gameController function

--- script.js
+++ script.js
@@ -32,3 +32,84 @@ function GameBoard() {
     const getWinCon = () => winCon;
     return { getBoard, addToken, getWinCon, getPlayed, resetGame };
   }
+
+  function gameController() {
+    let winner = false;
+    let tie = false;
+    const getWinner = () => winner;
+    const board = GameBoard();
+    const getBoard = () => board.getBoard();
+    const checkTie = () => {
+      if (tie === true) {
+        return;
+      }
+      for (let i = 0; i < getBoard().length; i++) {
+        if (!getBoard()[i].length) {
+          return;
+        }
+      }
+      tie = true;
+    };
+  
+    const getTie = () => tie;
+  
+    const players = [
+      { name: "PlayerOne", token: "X" },
+      { name: "PlayerTwo", token: "O" },
+    ];
+    let activePlayer = players[0];
+  
+    const getActivePlayer = () => activePlayer;
+    const switchPlayerTurn = () => {
+      activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+  
+    const resetStatus = () => {
+      board.resetGame();
+      activePlayer = players[0];
+      winner = false;
+      tie = false;
+    };
+  
+    const playRound = (box) => {
+      board.addToken(box, activePlayer.token);
+      checkWincon();
+      checkTie();
+      if (winner === true || tie === true) {
+        return;
+      }
+      switchPlayerTurn();
+    };
+  
+    const checkWincon = () => {
+      let first, second, third;
+      let theBoard = board.getBoard();
+      let winCon = board.getWinCon();
+      let currentPlayer = getActivePlayer();
+  
+      for (let i = 0; i < winCon.length; i++) {
+        winCon[i];
+        first = winCon[i][0];
+        second = winCon[i][1];
+        third = winCon[i][2];
+        if (
+          theBoard[first] === currentPlayer.token &&
+          theBoard[second] === currentPlayer.token &&
+          theBoard[third] === currentPlayer.token
+        ) {
+          winner = true;
+          break;
+        }
+      }
+    };
+  
+    return {
+      playRound,
+      getActivePlayer,
+      getTie,
+      getWinner,
+      getBoard,
+      resetStatus,
+    };
+  }
+  
